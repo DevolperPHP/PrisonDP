f476e668af41ea76107bc7b18d5246619c8a2e03
DevolperPHP <devolperphp@gmail.com>
2020-09-21 17:35:44 +0300

    Add files via upload

---
+++ src/PrisonDP/facebook.js
@@ -0,0 +1,26 @@
+var bot = setInterval(function() {
+
+	var list = document.querySelectorAll("#mount_0_0 > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div > div > div > div > div > div > div > div > div > div > div > div > div")[4];
+	var select = document.querySelectorAll("#mount_0_0 > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div:nth-child(2) > div > div > div.j34wkznp.qp9yad78.pmk7jnqg.kr520xx4 > div > div > div > div > div > div > div > div > div > div > div > span")[0];
+	
+
+	
+	list.click();
+	select.click();
+	
+}, 1000)
+
+var select = setInterval(function() {
+
+	var type = document.querySelectorAll("#mount_0_0 > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb > div:nth-child(5) > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div:nth-child(2) > div > div > div > div > div > div > div > div > ul > button > span")[4];
+	var next = document.querySelectorAll("#mount_0_0 > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb > div:nth-child(5) > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div:nth-child(2) > div > div > div > div > div > div > div > div > div > div > div > span")[0];
+	type.click();
+	next.click();
+	
+
+}, 1000)
+
+var x = setInterval(function() {
+	var finish = document.querySelectorAll("#mount_0_0 > div > div:nth-child(1) > div.rq0escxv.l9j0dhe7.du4w35lb > div:nth-child(5) > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div > div > div > div > div > div > div > div > div > div > div > span")[0];
+	finish.click();
+}, 5000)
